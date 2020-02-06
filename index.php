1cf464055032584875c93b045b4a2c4d2b4bd0b3
Saurav Shrestha <sauravxtha@gmail.com>
2020-02-06 15:37:33 +0545

    updated [agination

--- index.php
+++ index.php
@@ -51,7 +51,13 @@ if ( is_home() ) {
 						endwhile;
 
 					if ( get_theme_mod( 'blog_pagination_mode' ) === 'numeric' ) {
-						the_posts_pagination();
+						the_posts_pagination(
+							array(
+								'mid_size'  => 2,
+								'prev_text' => __( '<', 'textdomain' ),
+								'next_text' => __( '&#62', 'textdomain' ),
+							)
+						);
 					} else {
 						the_posts_navigation();
 					}
--- search.php
+++ search.php
@@ -41,7 +41,11 @@ get_header();
 
 					<?php
 					if ( get_theme_mod( 'blog_pagination_mode' ) === 'numeric' ) {
-						the_posts_pagination();
+						the_posts_pagination( array(
+							'mid_size'  => 2,
+							'prev_text' => __( '<', 'textdomain' ),
+							'next_text' => __( '&#62', 'textdomain' ),
+						) );
 					} else {
 						the_posts_navigation();
 					}
