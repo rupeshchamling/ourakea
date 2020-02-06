25fd793363444e8c844dea28bfb52a62bb8ef4fe
Saurav Shrestha <sauravxtha@gmail.com>
2020-02-06 16:26:10 +0545

    updated headers

--- inc/helpers.php
+++ inc/helpers.php
@@ -22,51 +22,92 @@ if ( ! function_exists( 'ourakea_header_page_title' ) ) :
 	 * @since 1.0.0
 	 */
 	function ourakea_header_page_title() {
-		if ( is_front_page() ) :
-			return;
-		elseif ( is_home() ) :
-			?>
+
+		if ( have_posts() ) {
+			if ( is_front_page() ) :
+				return;
+				elseif ( is_home() ) :
+					?>
 <div class="page-content align-center">
-    <div class="container">
-        <h1 class="header-heading"><?php single_post_title(); ?></h1>
-    </div>
+	<div class="container">
+		<h1 class="header-heading"><?php single_post_title(); ?></h1>
+	</div>
 </div>
-<?php
-		elseif ( is_archive() ) :
-			?>
+					<?php
+				elseif ( is_archive() ) :
+					?>
 <div class="page-content">
-    <div class="container">
-        <h1 class="header-heading"><?php the_archive_title(); ?></h1>
-    </div>
+	<div class="container">
+		<h1 class="header-heading"><?php the_archive_title(); ?></h1>
+	</div>
 </div>
 
-<?php
-		elseif ( is_search() ) :
-			?>
+					<?php
+				elseif ( is_search() ) :
+					?>
 <div class="page-content">
-    <div class="container">
-        <h1 class="header-heading">
-            <?php printf( esc_html__( 'Search Results for: %s', 'ourakea' ), get_search_query() ); ?>
-        </h1>
-    </div>
+	<div class="container">
+		<h1 class="header-heading">
+						<?php printf( __( 'Search Results <span>%s</span>', 'ourakea' ), get_search_query() ); ?>
+		</h1>
+	</div>
 </div>
-<?php
-		elseif ( is_404() ) :
-			?>
+					<?php
+				endif;
+		} else {
+			if ( is_404() ) :
+				?>
 <div class="page-content">
-    <div class="container">
-        <h1 class="header-heading">
-            <span><?php echo __( 'Oops!', 'ourakea' ); ?></span><?php echo esc_html__( ' That page can&#39;t be found.', 'ourakea' ); ?>
-        </h1>
+	<div class="container">
+		<h1 class="header-heading">
+			<span><?php echo __( 'Oops!', 'ourakea' ); ?></span><?php echo esc_html__( ' That page can&#39;t be found.', 'ourakea' ); ?>
+		</h1>
 
-        <div class="error-404 not-found">
-            <?php get_search_form(); ?>
-        </div>
-    </div>
+		<div class="error-404 not-found">
+					<?php get_search_form(); ?>
+		</div>
+	</div>
 </div>
+			<?php else : ?>
+		<div class="page-content">
+	<div class="container">
+		<h1 class="header-heading"><?php esc_html_e( 'Nothing Found', 'ourakea' ); ?></h1>
+				<?php
+				if ( is_home() && current_user_can( 'publish_posts' ) ) :
 
-<?php
-		endif;
+					printf(
+						'<span>' . wp_kses(
+						/* translators: 1: link to WP admin new post page. */
+							__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'ourakea' ),
+							array(
+								'a' => array(
+									'href' => array(),
+								),
+							)
+						) . '</span>',
+						esc_url( admin_url( 'post-new.php' ) )
+					);
+
+				elseif ( is_search() ) :
+					?>
+
+					<span><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'ourakea' ); ?></span>
+					<?php
+					get_search_form();
+
+						else :
+							?>
+
+					<span><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'ourakea' ); ?></span>
+							<?php
+							get_search_form();
+
+						endif;
+						?>
+	</div>
+</div>
+				<?php
+				endif;
+		}
 	}
-
 endif;
