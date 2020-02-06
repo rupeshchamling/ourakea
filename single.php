ffc82613a20f20f362bad5a07b9e19a12abfefa1
Saurav Shrestha <sauravxtha@gmail.com>
2020-02-06 12:27:00 +0545

    updated related post

--- single.php
+++ single.php
@@ -55,6 +55,54 @@ get_header();
 				</div>
 			</nav>
 			<?php } ?>
+
+				<?php
+				// Query random posts.
+				$the_query = ourakea_related_post_query( get_the_ID(), 5 );
+				?>
+				<?php
+				// If we have posts lets show them.
+				if ( $the_query->have_posts() ) :
+					?>
+					<div class="related_wrap">
+						<h2 class="section-title"><span><?php esc_html_e( 'Related Posts', 'ourakea' ); ?></span></h2>
+
+						<?php
+						// Loop through the posts.
+						while ( $the_query->have_posts() ) :
+							$the_query->the_post();
+							?>
+							<div class="row_wrap">
+								<article class="row">
+									<div class="col-sm-5 figure">
+										<a href="<?php the_permalink(); ?>">
+											<?php ourakea_post_thumbnail(); ?>
+										</a>
+									</div>
+
+									<div class="blog_content col-sm-7">
+										<?php
+										$categories_list = get_the_category_list( ' ' );
+										if ( $categories_list ) {
+											/* translators: 1: list of categories. */
+											printf( '<span class="cat-links">' . esc_html__( '%1$s', 'ourakea' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+										}
+
+										$tags_list = get_the_tag_list( '', ' ' );
+										if ( $tags_list ) {
+											/* translators: 1: list of tags. */
+											printf( '<span class="tags-links">' . esc_html__( '%1$s', 'ourakea' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+										}
+										?>
+										<h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+										<span class='entry_date'><?php ourakea_posted_on(); ?></span>
+									</div>
+								</article>
+							</div>
+						<?php endwhile; ?>
+						<?php wp_reset_postdata(); ?>
+					</div>
+				<?php endif; ?>
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
